42c35e8878566bba1a6c71fc8eed58b0a4f0429e
Soumyajit <64969783+Sam7540@users.noreply.github.com>
2020-06-26 19:56:37 +0530

    Add files via upload

---
+++ JAVA- Scope (Large difference of numbers)_(Hacker_Rank)/Makedif.java
@@ -0,0 +1,46 @@
+package hackerrank;
+import java.util.*;
+
+
+class Difference {
+  	private int[] elements;
+  	public int maximumDifference;
+    int first, tempdiff, tempmax=0;
+    public Difference(int[] x)
+    {
+        elements = x;
+    }
+    public void computeDifference()
+    {
+        for(int i=0; i<elements.length-1; i++)
+        {
+            first = elements[i];
+            for(int j=i+1; j<elements.length; j++)
+            {
+            	tempdiff = Math.abs(first - elements[j]);
+                if( tempdiff > tempmax)  tempmax = tempdiff;
+            }
+        }
+        maximumDifference = tempmax;
+    }
+
+} // End of Difference class
+
+public class Makedif {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        sc.close();
+
+        Difference difference = new Difference(a);
+
+        difference.computeDifference();
+
+        System.out.print(difference.maximumDifference);
+    }
+}
